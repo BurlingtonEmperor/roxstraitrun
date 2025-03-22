8d8f77b9c5db2cbb525ef56cf60e38e2e008070a
BurlingtonEmperor <150710529+BurlingtonEmperor@users.noreply.github.com>
2025-03-22 00:29:22 +0000

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = require('express')();
+const http = require('http').Server(app);
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+app.get('', function (req, res) {
+  const index = __dirname + '/public/static/index.html';
+
+  res.sendFile(index);
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
